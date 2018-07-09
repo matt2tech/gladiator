3f2662624318e101896a4acd297465d55a1ababf
matt2tech <mlipsey@basecampcodingacademy.org>
2018-07-09 15:22:08 -0500

    second function is done just need to test it

--- core.py
+++ core.py
@@ -9,3 +9,14 @@ def new_gladiator(health, rage, damage_low, damage_high):
         'damage_high': damage_high
     }
     return gladiator1
+
+
+def attack(attacker, defender):
+    attack = randint(attacker['damage_low'], attacker['damage_high'])
+    crit_chance = randint(attack['rage'], 100)
+    if crit_chance == 100:
+        defender['health'] = defender['health'] - attack * 2
+        attacker['rage'] = 0
+    else:
+        defender['health'] = defender['health'] - attack
+        attacker['rage'] += 15
