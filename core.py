01730e693c86b808115995819fbaebdc7272cc1a
matt2tech <mlipsey@basecampcodingacademy.org>
2018-07-09 16:16:09 -0500

    all functions have been tested

--- core.py
+++ core.py
@@ -16,22 +16,31 @@ def attack(attacker, defender):
     if attacker['rage'] >= randint(1, 100):
         defender['health'] = max(defender['health'] - attack * 2, 0)
         attacker['rage'] = 0
-        return defender['health']
+        #testing function
+        #return defender['health']
     else:
         defender['health'] = max(defender['health'] - attack, 0)
         attacker['rage'] += 15
-        return defender['health']
+        #testing function
+        #return defender['health']
 
 
 def heal(gladiator):
     if gladiator['rage'] >= 20:
         gladiator['health'] = min(gladiator['health'] + 10, 100)
         gladiator['rage'] -= 20
-        return gladiator['health']
+        #testing function
+        #return gladiator['health']
     else:
         print('Insufficient rage')
-        return 'Insufficient rage'
+        #testing function
+        #return 'Insufficient rage'
 
 
 def is_dead(gladiator):
     gladiator['health'] == 0
+    #testing function
+    #if gladiator['health'] == 0:
+    #    return True
+    #else:
+    #    return False
