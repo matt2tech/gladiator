853214b5d9c7d777aa4eb95166c193146b545750
matt2tech <mlipsey@basecampcodingacademy.org>
2018-07-09 15:30:55 -0500

    sean gave me advice on a better critical chance

--- core.py
+++ core.py
@@ -13,10 +13,11 @@ def new_gladiator(health, rage, damage_low, damage_high):
 
 def attack(attacker, defender):
     attack = randint(attacker['damage_low'], attacker['damage_high'])
-    crit_chance = randint(attack['rage'], 100)
-    if crit_chance == 100:
+    if attacker['rage'] > randint(1, 100):
         defender['health'] = defender['health'] - attack * 2
         attacker['rage'] = 0
+        return defender['health']
     else:
         defender['health'] = defender['health'] - attack
         attacker['rage'] += 15
+        return defender['health']
