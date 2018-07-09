d6b62a9aea8d0f0a8720306828c09c80c658e105
matt2tech <mlipsey@basecampcodingacademy.org>
2018-07-09 15:09:40 -0500

    got the first function done

---
+++ core.py
@@ -0,0 +1,11 @@
+from random import randint
+
+
+def new_gladiator(health, rage, damage_low, damage_high):
+    gladiator1 = {
+        'health': health,
+        'rage': rage,
+        'damage_low': damage_low,
+        'damage_high': damage_high
+    }
+    return gladiator1
