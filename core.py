ecb2a295a792472e0e5ddf45626b0e40dd146188
matt2tech <mlipsey@basecampcodingacademy.org>
2018-07-09 16:09:49 -0500

    fourth function is built and needs to be tested

--- core.py
+++ core.py
@@ -13,12 +13,12 @@ def new_gladiator(health, rage, damage_low, damage_high):
 
 def attack(attacker, defender):
     attack = randint(attacker['damage_low'], attacker['damage_high'])
-    if attacker['rage'] > randint(1, 100):
-        defender['health'] = defender['health'] - attack * 2
+    if attacker['rage'] >= randint(1, 100):
+        defender['health'] = max(defender['health'] - attack * 2, 0)
         attacker['rage'] = 0
         return defender['health']
     else:
-        defender['health'] = defender['health'] - attack
+        defender['health'] = max(defender['health'] - attack, 0)
         attacker['rage'] += 15
         return defender['health']
 
@@ -27,5 +27,11 @@ def heal(gladiator):
     if gladiator['rage'] >= 20:
         gladiator['health'] = min(gladiator['health'] + 10, 100)
         gladiator['rage'] -= 20
+        return gladiator['health']
     else:
         print('Insufficient rage')
+        return 'Insufficient rage'
+
+
+def is_dead(gladiator):
+    gladiator['health'] == 0
