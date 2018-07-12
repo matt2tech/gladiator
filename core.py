754da85ee63952b7dd98a750ace8bccccec6351c
matt2tech <mlipsey@basecampcodingacademy.org>
2018-07-12 10:31:57 -0500

    fixed an rage error

--- core.py
+++ core.py
@@ -34,7 +34,7 @@ def attack(attacker, defender, player, other_player):
     else:
         print('{} missed'.format(player))
         print('( ÒДÓ)⊃o-|===> ε=ε=ε=┌( ^-^)ﾉ')
-        attacker['rage'] -= 10
+        attacker['rage'] = max(attacker['rage'] - 10, 0)
 
 
 def heal(gladiator, name):
